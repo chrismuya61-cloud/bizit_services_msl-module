9d7a0f8164081159acc6c543e43912baa521d284
chrismuya61-cloud <chrismuya61@gmail.com>
2025-12-01 21:16:05 +0300

    Update manage_sales_list.php

--- bizit_services_msl_Version 3-Enhanced/bizit_services_msl/views/admin/services/manage_sales_list.php
+++ bizit_services_msl_Version 3-Enhanced/bizit_services_msl/views/admin/services/manage_sales_list.php
@@ -1,31 +1,35 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <?php init_head(); ?>
 <div id="wrapper">
-  <div class="content">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="panel_s">
-          <div class="panel-body">
-            <h4>Services Sales List</h4>
-            <hr/>
-            <div class="clearfix"></div>
-            <?php render_datatable(array(
-              _l('service_code'), 
-              _l('services_title'), 
-              _l('service_type'), 
-              _l('selling_price'),
-              ),'services_sales_list'); ?>
+    <div class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel_s">
+                    <div class="panel-body">
+                        <h4 class="no-margin"><?php echo $title; ?></h4>
+                        <hr class="hr-panel-heading" />
+                        
+                        <div class="clearfix"></div>
+                        
+                        <?php render_datatable(array(
+                            'Service / Item',
+                            'Code',
+                            'Category',
+                            'Price',
+                            'Date Created'
+                        ),'services_sales_list'); ?>
+                        
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
-
+</div>
 <?php init_tail(); ?>
 <script>
-  $(function(){
-    initDataTable('.table-services_sales_list', admin_url + 'services/sales_list', [], [],'undefined',[0,'ASC']);
-  });
+    $(function(){
+        initDataTable('.table-services_sales_list', window.location.href, [], [], 'undefined', [4, 'DESC']);
+    });
 </script>
 </body>
 </html>
