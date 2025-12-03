299cf0cf8ceca8144a7a84900116be0ba99223c8
chrismuya61-cloud <chrismuya61@gmail.com>
2025-12-03 23:14:14 +0300

    Update manage_requests.php

--- bizit_services_msl_Version 3-Enhanced/bizit_services_msl/views/admin/services/manage_requests.php
+++ bizit_services_msl_Version 3-Enhanced/bizit_services_msl/views/admin/services/manage_requests.php
@@ -3,18 +3,29 @@
   <div class="content">
     <div class="row">
       <div class="col-md-12">
-        <?php if(has_permission(BIZIT_SERVICES_MSL,'','create')){ ?>
-        <div class="panel_s">
-          <div class="panel-body _buttons">
-            <a href="<?= admin_url('services/new_request'); ?>" class="btn btn-info pull-left"><i class="fa fa-plus"></i> <?php echo _l('new_service_request'); ?></a>
-          </div>
-
-        </div>
-        <?php } ?>
+        
         <div class="panel_s">
           <div class="panel-body">
+            <div class="_buttons">
+              <?php if(has_permission(BIZIT_SERVICES_MSL,'','create')){ ?>
+                  <a href="<?= admin_url('services/new_request'); ?>" class="btn btn-info pull-left display-block">
+                      <i class="fa fa-plus-circle"></i> <?php echo _l('new_service_request'); ?>
+                  </a>
+              <?php } ?>
+              <div class="clearfix"></div>
+            </div>
+            
+            <hr class="hr-panel-heading" />
+            
             <div class="clearfix"></div>
-              <?php render_datatable(array(
+            
+            <div class="row mbot15">
+                <div class="col-md-12">
+                    <h4 class="no-margin"><?php echo _l('als_services_requests'); ?> List</h4>
+                </div>
+            </div>
+
+            <?php render_datatable(array(
               _l('request_code'), 
               _l('client'),  
               _l('item'), 
@@ -24,16 +35,16 @@
               _l('status'),
               _l('options'),
               ),'services_requests'); ?>
-            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
+</div>
 <?php init_tail(); ?>
 <script>
   $(function(){
-    initDataTable('.table-services_requests', admin_url + 'services/requests', [7], [7],'undefined',[3,'DESC']);
+    initDataTable('.table-services_requests', admin_url + 'services/requests', [7], [7],'undefined',[0,'DESC']);
   });
 </script>
 </body>
